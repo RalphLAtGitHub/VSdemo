3c553eee7052356396bd6e8b319eff353dfc7819
RalphLAtGitHub <mail@ralphlechterbeck.de>
2023-04-21 15:58:33 +0200

    Update test.js

--- Babylon/WebResources/test.js
+++ Babylon/WebResources/test.js
@@ -12,6 +12,6 @@ var view = new ol.View({
     zoom: 3,
     maxZoom: 18,
     minZoom: 3,
-    center: ol.proj.fromLonLat([0.0045, 50.4095]), // Greenwich Lightvessel
+    center: ol.proj.fromLonLat([0.0045, 50.4095])
 });
 map.setView(view);
