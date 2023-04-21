7a8065288ebc9199a095d41303a89d5a269ca854
RalphLAtGitHub <mail@ralphlechterbeck.de>
2023-04-21 15:55:05 +0200

    Add files via upload

---
+++ Babylon/WebResources/test.js
@@ -0,0 +1,17 @@
+
+var osmLayer = new ol.layer.Tile({
+    source: new ol.source.OSM()
+});
+
+var map = new ol.Map({
+    layers: [osmLayer],
+    target: "map"
+});
+
+var view = new ol.View({     
+    zoom: 3,
+    maxZoom: 18,
+    minZoom: 3,
+    center: ol.proj.fromLonLat([0.0045, 50.4095]), // Greenwich Lightvessel
+});
+map.setView(view);
---
+++ Babylon/WebViewForm.Designer.vb
@@ -0,0 +1,39 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class WebViewForm
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' WebViewForm
+        ' 
+        AutoScaleDimensions = New SizeF(96F, 96F)
+        AutoScaleMode = AutoScaleMode.Dpi
+        ClientSize = New Size(491, 294)
+        DoubleBuffered = True
+        MdiChildrenMinimizedAnchorBottom = False
+        Name = "WebViewForm"
+        StartPosition = FormStartPosition.CenterScreen
+        ResumeLayout(False)
+    End Sub
+
+End Class
